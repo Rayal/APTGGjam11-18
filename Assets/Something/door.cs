d8118536ccc91c4260a02ff7b4abc6cbb20b382e
Matter12 <arturs.svars@gmail.com>
2018-11-03 14:38:55 +0200

    Doors and house

---
+++ Assets/Something/door.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour {
+
+    public Sprite closedDoor;
+    public Sprite openDoor;
+
+    private SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () {
+        sr = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        sr.sprite = openDoor;
+        print(collision.gameObject.name);
+    }
+}
