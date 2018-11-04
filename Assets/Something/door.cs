92f3fa720778933f1f677de1342a97d10e4c100a
Matter12 <arturs.svars@gmail.com>
2018-11-04 10:05:50 +0200

    no message

--- Assets/Something/door.cs
+++ Assets/Something/door.cs
@@ -11,16 +11,22 @@ public class door : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        sr = GetComponent<SpriteRenderer>();
+        //sr = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        sr = GetComponent<SpriteRenderer>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        sr.sprite = openDoor;
-        print(collision.gameObject.name);
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            sr.sprite = openDoor;
+            print(collision.gameObject.name);
+            transform.Translate(Vector2.right * 1.3f);
+            Destroy(GetComponent<BoxCollider2D>());
+        }
+        
     }
 }
